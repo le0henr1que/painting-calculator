42deff206ad077d5a6e44f86204d47ce16a8ff08
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-04-20 09:24:32 -0300

    feat: resolve origin

--- src/modules/area/useCase/CalculatorAreaController.ts
+++ src/modules/area/useCase/CalculatorAreaController.ts
@@ -5,6 +5,7 @@ export class CalculatorAreaController {
   constructor(private calculatorAreaUseCase: CalculatorAreaUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
+    console.log('Teste');
     await this.calculatorAreaUseCase.execute();
     return response.status(201).json({ error: false });
   }
